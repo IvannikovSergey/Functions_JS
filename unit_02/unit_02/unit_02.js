8327f2e00535f9ae265095e8b58076864e1ac489
nagoy <nagoymail@gmail.com>
2022-01-26 10:53:54 +0500

    юнит 2 таск 6 переделан

--- unit_02/unit_02/unit_02.js
+++ unit_02/unit_02/unit_02.js
@@ -76,7 +76,7 @@ document.querySelector('.b-5').addEventListener('click', function () {
 const out6 = document.querySelector('.out-6');
 
 function t6(arr, block) {
-    block.textContent = arr;
+    block.textContent = arr.join(' ');
 }
 
 document.querySelector('.b-6').addEventListener('click', function () {
